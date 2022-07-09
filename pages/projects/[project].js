fa9880da4b7af666d6066d85bf260f4645216ebd
Simon Calle Laverde <kyverde88@gmail.com>
2022-07-08 23:59:06 -0500

    Summarizing (& Re-doing) Everything I Did In "Portfolio V3 2022 NextJS". Continuing Dynamic Routes.
    - Implementing "getStaticProps()" function to fetch data.
    - Destructuring "car" prop to use it in the JSX.
    º Adding <img/> tag which relies on the data fetched by the server as the image "src".
    - "import Head from 'next/head'" for SEO.
    - Implementing "getStaticPaths()" function, to let NextJS know which dynamic pages to render.
    º "fallback: false" to ensure a URL that doesn't exist in the paths, redirects to a "404" page.
    
    -- Vid 10:10.

--- pages/projects/[project].js
+++ pages/projects/[project].js
@@ -2,68 +2,63 @@
 
 // Imports
 import { useRouter } from "next/router";//Access query parameters from the URL
-import Head from "next/head";//......
+import Head from "next/head";
 
 // Project (Page) (Dynamic) Component
-// (WILL BE THE DYNAMIC "ProjectPage" SOON)
-export default function CarPage({ car }) {//Destructuring "car" prop to use in "CarPage" component
+export default function ProjectPage({ car }) {//Destructuring "car" prop to use here in "ProjectPage" component
 	const router = useRouter()
 	const { project } = router.query//Gets whatever is written in the URL parameter after "/projects/"
 
-	return <h1>Hello {project}</h1>//"{project}" prints out the URL parameter after "/projects/" (e.g. "/projects/{project}")
+	return (
+		<>
+			<Head>
+				<title>{car.color} {car.id}</title>
+				{/* To add meta-tags, etc */}
+			</Head>
 
+			{/* "{project}" prints out the URL parameter after "/projects/" (e.g. "/projects/tesla") */}
+			<h1>Hello {project}</h1>
 
-
-	// return (
-	// 	<>
-	// 		<Head>
-	// 			<title>{car.color} {car.id}</title>
-	// 		</Head>
-
-	// 		<h1>Hello {project}</h1>
-
-	// 		<img src={car.image} alt="" width="300px"/>
-	// 	</>
-	// )
+			<img src={car.image} alt="" width="500"/>{/*300px*/}
+		</>
+	)
 };
 
+// Fetching Data (On "Build") // Tells Next To Pre-Render Page
+// Sends The Result As Props To The Component Itself
+export async function getStaticProps({ params }) {
+	// Fetching the JSON for an individual car // We need the "id" from the URL to know which car was requested
+	// Getting that info from the "params" argument in the function // Requesting the JSON file with that "id"
+	const req = await fetch(`http://localhost:3000/${params.project}.json`)
 
+	// Converting to JSON
+	const data = await req.json()
 
-// // Fetching Data // On "Build"
-// export async function getStaticProps({ params }) {
-// 	// Fetching the Json for an individual car // Getting the 'id' from the URL
-// 	const req = await fetch(`http://localhost:3000/${params.id}.json`)
+	// Returning an object that has a "props" property, where each "prop" can then be accessed by the component
+	return {
+		props: { car: data },
+	}
+};
 
-// 	// Converting to Json
-// 	const data = await req.json()
+// Tells NextJS Which Dynamic Pages To Render
+export async function getStaticPaths() {//{ params }
+	// Can also request data from an API or Data-Base
+	const req = await fetch("http://localhost:3000/cars.json")
 
-// 	// Returning an object that has a 'props' property
-// 	// where each 'prop' can then be accessed by the component
-// 	return {
-// 		props: { car: data },
-// 	}
-// };
+	// Converting to JSON
+	const data = await req.json()
 
-// // Tells Next which dynamic pages to render
-// export async function getStaticPaths({ params }) {
-// 	// Can also request data from API or Data-Base
-// 	const req = await fetch("http://localhost:3000/cars.json")
+	// Returns a "paths" object, that contains an array with every route for this dynamic URL
+	const paths = data.map(car => {
+		return { params: { project: car } }//id: car
+	})
 
-// 	// Converting to Json
-// 	const data = await req.json()
-
-// 	// Returns paths object that contains an array with every route for this dynamic URL
-// 	const paths = data.map(car => {
-// 		return { params: { id: car }}
-// 	})
-
-// 	// Returning routes (tesla, ford, lambo) mapped to an array of objects
-// 	// Along with 'fallback: false' behavior (for 404 pages)
-// 	return {
-// 		paths,
-// 		fallback: false
-// 	}
-// };
+	// We map our route values (tesla, ford, lambo) to an array of objects (above), then return them from the function, along with additional options like the fallback behavior
+	return {
+		paths,
+		fallback: false//Ensures a URL that doesn't exist in the paths, redirects to a "404" page
+	}
+};
 
 
 
