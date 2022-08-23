0880db66e96aa9d8ec919a74d655ba8d944915dc
Simon Calle Laverde <kyverde88@gmail.com>
2022-08-23 17:24:23 -0500

    "HomePage" Base Structure, Missing Styles Yet.
    - Mixing HomePage with my "portfolio-V3-2022-next-js's" HomePage:
    º To revise <Head/> metas there later, as it was throwing errors still last time. (Need to check many ReactJS/NextJS things there).
    º Missing styles yet.
    
    -------------------------------------
    -- Revising "Portfolio V3 2022 NextJS" to get there:
    º Homepage as it was there (but now with this fetched content already too, in the middle).
    º Base styles structure as I had already there.
    º Fonts as I had there.
    -------------------------------------

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 // Imports
 import Head from "next/head";
-//import styles from "../styles/HomePage.module.scss";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/HomePage.module.scss";//I'm Modifying
 import { GraphQLClient, gql } from "graphql-request";
 import ProjectCard from "../components/ProjectCard";
 
-// THIS PART BELOW IS BEING USED TO FETCH USING GRAPHCMS
+// THIS PART BELOW IS FETCHING CONTENT USING GRAPHCMS [START]
 
 // API Access Endpoint Token (Found at: "GraphCMS > Project > Project Settings > API Access > Content API")
 const accessEndpoint = "https://api-us-east-1.hygraph.com/v2/cl5ketcvx2wnm01ta90nhcdmy/master";
@@ -57,41 +59,76 @@ export async function getStaticProps() {
   }
 };
 
+// THIS PART ABOVE IS FETCHING CONTENT USING GRAPHCMS [END]
+
 // Home (Page) Component
-// Passing "allProjects" As Props
-// Passing Down Data Into "ProjectCard"
 export default function HomePage({ allProjects }) {
   return (
-    <>
+    <>{/*className={ styles.container }*/}
+
       <Head>
         <title>Portfolio V3 2022 NextJS</title>
         <meta name="description" content="Portfolio V3 2022 NextJS using GraphCMS."/>
         <link rel="icon" href="/favicon.ico"/>
+
+        {/* Fonts */}
+        {/*<link href="https://db.onlinewebfonts.com/c/4c4c08af466e9ad071b6d69cf44093df?family=Saol+Display+Regular" rel="stylesheet" type="text/css"/>*/}{/*Option 2: "Old+Standard+TT"*/}
       </Head>
 
-      <main>
-        {/* Mapping through "allProjects" and displaying each "project", in a "ProjectCard" component */}
-        {allProjects.map(project => (
-          <ProjectCard
-            key={project.id}
-            title={project.title}
-            slug={project.slug}
-            nameForThumbnail={project.nameForThumbnail}
-            client={project.client}
-            thumbnailImage={project.thumbnailImage}
-            headerImage={project.headerImage}
-            platforms={project.platforms}
-            year={project.year}
-            roles={project.roles}
-            webLaunchUrl={project.webLaunchUrl}
-            webImages={project.webImages}
-            type={project.type}
-            category={project.category}
-            tags={project.tags}
-            content={project.content}
-          />
-        ))}
+      <header className="display-flex justify-content-center align-items-center">{/*flex-direction-row*/}
+        <div className="container">
+          {/* Image Component Example Temporary */}
+          {/*<span className={ styles.logo }>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16}/>
+          </span>*/}
+
+          {/* Temporary */}
+          <h1 className="title">{/*{styles.title}*/}
+            Portfolio V3 2022 Next.js
+          </h1>
+        </div>
+      </header>
+
+      {/* Temporary */}
+      <main>{/*{ styles.main }*/}
+        <div className="container">
+          {/* Mapping through "allProjects" and displaying each "project", in a "ProjectCard" component */}
+          {allProjects.map(project => (
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              slug={project.slug}
+              nameForThumbnail={project.nameForThumbnail}
+              client={project.client}
+              thumbnailImage={project.thumbnailImage}
+              headerImage={project.headerImage}
+              platforms={project.platforms}
+              year={project.year}
+              roles={project.roles}
+              webLaunchUrl={project.webLaunchUrl}
+              webImages={project.webImages}
+              type={project.type}
+              category={project.category}
+              tags={project.tags}
+              content={project.content}
+            />
+          ))}
+        </div>
       </main>
+
+      {/* Temporary */}
+      <footer className="display-flex justify-content-center align-items-center">{/*{styles.footer}*/}
+        <div className="container">
+          <span>
+            Designed & Developed by{" "}
+          </span>
+
+          <a href="https://simoncallelaverde.github.io/" target="_blank">
+            Simón Calle Laverde
+          </a>
+        </div>
+      </footer>
+
     </>
   )
 };
