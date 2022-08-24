4a5f0066e3cc21e15cbe59a20dc947b6b0449ad8
Simon Calle Laverde <kyverde88@gmail.com>
2022-08-23 23:42:11 -0500

    TO-DOS.
    - Small edits.
    
    - Revised "Portfolio V3 2022 NextJS" to get to:
    º Homepage as it was there (but now with this fetched content already too, in <main/>). √
    º Base styles structure as I had there. √
    º Fonts as I had there. √
    
    -------------------------------------
    -- Still need to remove/clean old & new styles a bit.
    -- Be-aware/investigate previous warning about not adding font in "<Head/>". Not sure if it isn't correct still.
    -- To check "<Head/>'s" metas, SEO, and everything as I was doing in "portfolio-v3-2022-next-js" previous time.
    
    -- To start with/re-creating "<footer></footer>" I guess, from my "Portfolio 2021" (in Jekyll). As it is the easiest, shared site-wide, and will break the ice to start styling in NextJS and creating a site. (Also might change everything else, but love my old footer and might start with a similar one (for whatever this site ends like)).
    º Might check my social-media and stuff right away too, as it is all linked in my "footer".
    º I.e. "<header></header> has ages more work, animations, complex styles, JavaScripts, etc, and not sure how alike it-will/I-want-it-to remain/end-up yet.
    
    -- To fetch content under "pages/projects/index.js" & under dynamic "pages/projects/[project].js" as well.
    -------------------------------------

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -7,59 +7,74 @@ import styles from "../styles/ProjectCard.module.scss";
 export default function ProjectCard({ title, slug, nameForThumbnail, client, thumbnailImage, headerImage, platforms, year, roles, webLaunchUrl, webImages, type, category, tags, content }) {
 	return (
 		<article className={styles.article}>
+			<h1>Title: {title}</h1>
+
 			<Link href={`/projects/${slug}`}>
 				{/*<Image src={headerImage.url} alt={`${title} Project's Header Image`} layout="fill"/>*/}{/* Must use "width" & "height" properties or "layout='fill'" property */}
 				<img src={headerImage.url} alt={`${title} Project's Header Image`}/>
 			</Link>
 
 			<div className="project-caption">
-				<h1>Title: {title}</h1>
-				<h5>Client: {client}</h5>
-				<Image src={thumbnailImage.url} alt={`${title} Project's Thumbnail Image`} width="120" height="90"/>
-				<h5>Name For Thumbnail: {nameForThumbnail}</h5>
-				<h5>Year: {year}</h5>
-				<h5>Type: {type}</h5>
-				<h5>Category: {category}</h5>
-				<h5>Slug: {slug}</h5>
-				<h5>Web Launch URL: {webLaunchUrl}</h5>
-				<p>Content: {content.text}</p>
+				<h4>Client: {client}</h4>
+				<br/>
 
-				<h6 style={{marginBottom:"0px", color: "green"}}>Allows Multiple Values:</h6>
-				<h4 style={{marginTop:"0px"}}>
+				<Image src={thumbnailImage.url} alt={`${title} Project's Thumbnail Image`} width="120" height="90"/>
+				<h4>Name For Thumbnail: {nameForThumbnail}</h4>
+				<br/>
+
+				<h4>Year: {year}</h4>
+				<h4>Type: {type}</h4>
+				<h4>Category: {category}</h4>
+				<h4>Slug: {slug}</h4>
+				<h4>Web Launch URL: {webLaunchUrl}</h4>
+				<br/>
+
+				<p>Content: {content.text}</p>
+				<br/>
+
+				<h6 style={{marginBottom:"0px", color: "green"}}>Multiple Values:</h6>
+				<h5 style={{marginTop:"0px"}}>
 					{/*Web Image [Index "0"]:<br/>{webImages[0].url}*/}
-					Web Images [All]: {webImages.map(image => (
+					WEB IMAGES:
+					{webImages.map(image => (
 						<div key={image.url}>
 							{image.url}
 						</div>
 					))}
-				</h4>
+				</h5>
+				<br/>
 
-				<h6 style={{marginBottom:"0px", color: "green"}}>Allows Multiple Values:</h6>
-				<h4 style={{marginTop:"0px"}}>
-					Platforms [All]: {platforms.map(platform => (
+				<h6 style={{marginBottom:"0px", color: "green"}}>Multiple Values:</h6>
+				<h5 style={{marginTop:"0px"}}>
+					PLATFORMS:
+					{platforms.map(platform => (
 						<div key={platform}>
 							{platform}
 						</div>
 					))}
-				</h4>
+				</h5>
+				<br/>
 
-				<h6 style={{marginBottom:"0px", color: "green"}}>Allows Multiple Values:</h6>
-				<h4 style={{marginTop:"0px"}}>
-					Roles [All]: {roles.map(role => (
+				<h6 style={{marginBottom:"0px", color: "green"}}>Multiple Values:</h6>
+				<h5 style={{marginTop:"0px"}}>
+					ROLES:
+					{roles.map(role => (
 						<div key={role}>
 							{role}
 						</div>
 					))}
-				</h4>
+				</h5>
+				<br/>
 
-				<h6 style={{marginBottom:"0px", color: "green"}}>Allows Multiple Values:</h6>
-				<h4 style={{marginTop:"0px"}}>
-					Tags [All]: {tags.map(tag => (
+				<h6 style={{marginBottom:"0px", color: "green"}}>Multiple Values:</h6>
+				<h5 style={{marginTop:"0px"}}>
+					TAGS:
+					{tags.map(tag => (
 						<div key={tag}>
 							{tag}
 						</div>
 					))}
-				</h4>
+				</h5>
 			</div>
 
 			{/* TEMPORARY // TO REVISE "moment.js" FOR DATES // USED IN MY "next-js-blog-graphcms" */}
